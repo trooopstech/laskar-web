2a120e558c85649675187e96d53db581afb72dd4
anggardhanoano <anggardhanoano@gmail.com>
2021-10-28 11:21:41 +0700

    add loading animation when upload attachment

--- src/components/modules/Editor/Common/file.upload.tsx
+++ src/components/modules/Editor/Common/file.upload.tsx
@@ -1,3 +1,5 @@
+import Modal from "components/modules/Modal";
+import useModal from "components/modules/Modal/useModal";
 import useUploadAttachment from "hooks/useUploadAttachment";
 import { useRef } from "react";
 import { MdAttachFile } from "react-icons/md";
@@ -8,7 +10,8 @@ export interface UploadProps {
 
 const FileUploader = ({ setUrl }: UploadProps) => {
   const fileRef = useRef<HTMLInputElement>(null);
-  const { generatePreSignedUrl } = useUploadAttachment();
+  const { generatePreSignedUrl, loading } = useUploadAttachment();
+  const { closeModal } = useModal();
 
   const setUploadedUrl = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const attachment = await generatePreSignedUrl(
@@ -37,6 +40,14 @@ const FileUploader = ({ setUrl }: UploadProps) => {
       >
         <MdAttachFile className="text-xl text-gray-500 hover:text-gray-100 mr-2" />
       </button>
+      <Modal onClose={closeModal} open={loading ? 1 : 0} permanent>
+        <div className="py-4 px-16">
+          <div className=" flex justify-center items-center">
+            <div className="animate-spin rounded-full h-16 w-16 border-l-4 border-b-4 border-blue-500"></div>
+          </div>
+          <h1 className="mt-4 text-gray-200 text-xl">Uploading File...</h1>
+        </div>
+      </Modal>
     </>
   );
 };
