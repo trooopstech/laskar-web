8ca1c5dab0d17b7684f1946ec03598c644bb7bd3
anggardhanoano <anggardhanoano@gmail.com>
2021-11-07 14:36:14 +0700

    adapt with new notif schema

--- src/schema/notification/index.ts
+++ src/schema/notification/index.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const REGISTER_DEVICE_KEY = gql`
-  mutation RegisterDeviceKey($device_key: String!) {
-    registerDeviceKey(device_key: $device_key) {
+  mutation RegisterDeviceKey($device_key: String!, $device_id: String!) {
+    registerDeviceKey(device_key: $device_key, device_id: $device_id) {
       id
     }
   }
